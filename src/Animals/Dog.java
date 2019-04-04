cd40f0f919006b1fbe7699d7069b4f8489462b05
AlexMud <48650965+AlexMud@users.noreply.github.com>
2019-04-05 01:23:57 +0300

    Update Dog.java

--- src/Animals/Dog.java
+++ src/Animals/Dog.java
@@ -12,25 +12,25 @@ public class Dog extends Animal {
 
     public void run(int dRun) {
         if (dRun <= this.dRun) {
-            System.out.println(this.name + " has run " + dRun + " m");
+            System.out.println(this.name + " has run " + dRun + " m"); // пробежал
         } else {
-            System.out.println(this.name + " can't run such a distance");
+            System.out.println(this.name + " can't run such a distance"); // не может пробежать такую дистанцию
         }
     }
 
     public void swim(float dSwim){
         if (dSwim <= this.dSwim) {
-            System.out.println(this.name + " has swum " + dSwim + " m");
+            System.out.println(this.name + " has swum " + dSwim + " m"); // проплыл
         } else {
-            System.out.println(this.name + " can't swim such a distance");
+            System.out.println(this.name + " can't swim such a distance"); // не может проплыть такую дистанцию
         }
     }
 
     public void jump(float h){
         if(h <= this.h){
-            System.out.println(this.name + " has jumped at " + " m");
+            System.out.println(this.name + " has jumped at " + " m"); // прыгнул
         } else {
-            System.out.println(this.name + " can't jump that high");
+            System.out.println(this.name + " can't jump that high"); // не может прыгнуть так высоко
         }
     }
 }
