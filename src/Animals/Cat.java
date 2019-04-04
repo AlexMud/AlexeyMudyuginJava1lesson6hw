9266fd520885b45591127cb43d02a3967aca3552
AlexMud <48650965+AlexMud@users.noreply.github.com>
2019-04-05 01:23:00 +0300

    Update Cat.java

--- src/Animals/Cat.java
+++ src/Animals/Cat.java
@@ -11,21 +11,21 @@ public class Cat extends Animal {
 
     public void run(int dRun) {
         if (dRun <= this.dRun) {
-            System.out.println(this.name + " has run " + dRun + " m");;
+            System.out.println(this.name + " has run " + dRun + " m"); // пробежал
         } else {
-            System.out.println(this.name + " can't run such a distance");;
+            System.out.println(this.name + " can't run such a distance"); // не может пробежать такую дистанцию
         }
     }
 
     public void swim(float dSwim){
-        System.out.println(this.species + " can't swim, do they?");
+        System.out.println(this.species + " can't swim, do they?"); // кошки не плавают, не так ли?
     }
 
     public void jump(float h){
         if(h <= this.h){
-            System.out.println(this.name + " has jumped at " + " m");;
+            System.out.println(this.name + " has jumped at " + " m"); // прыгнул
         } else {
-            System.out.println(this.name + " can't jump that high");
+            System.out.println(this.name + " can't jump that high"); // не может прыгнуть так высоко
         }
     }
 }
